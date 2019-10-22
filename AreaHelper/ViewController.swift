e4a0ec4fb82682094819272ed4ce08292220ed71
Chelsea Yue <cyue@lcs.on.ca>
2019-10-22 13:58:40 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Yue, Chelsea on 2019/10/22.
+//  Copyright © 2019 Chelsea Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
